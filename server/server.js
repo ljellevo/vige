13631466a038c38db18aa0556b376515593b9005
Ludvig Ellevold <21200123+ljellevo@users.noreply.github.com>
2020-01-07 23:29:43 +0100

    Working on server

--- server/server.js
+++ server/server.js
@@ -9,13 +9,19 @@ app.use(cors())
 app.use(express.json());
 //app.use("*", express.static(path.join(__dirname, '/../bin/index.html')));
 
+/*
 
 app.use(express.static('bin'));
+*/
+app.use("/", express.static('bin'));
+
+//app.use("/widgets", express.static('bin'));
 //app.use(express.static(path.join(__dirname, '/../bin')));
 
 
 const rest = require("./maintenance")(app);
 const api = require("./api")(app); 
+
 
 
 /*
@@ -36,18 +42,24 @@ app.get('/widgets/category', function(req, res, next){
   res.sendFile(path.resolve(__dirname + '/../bin/index.html'));
   //res.render(path.resolve(__dirname + '/../bin/index.html'));
 });
+
+app.use("/*", function(req, res) {
+    //resp.sendFile("/../bin/index.html");
+    res.sendFile(path.resolve(__dirname + '/../bin/index.html')); 
+});
 */
 
-app.get(['/*', '/widgets/*'], function(req, res, next){
+app.get("*", function(req, res, next){
   //res.redirect("/");
   console.log("page was requested"); 
-  //console.log(path.resolve(__dirname + '/../bin'));
+  console.log(path.resolve(__dirname + '/../bin/index.html'));
   //app.use(express.static('bin'));
-  //res.sendFile(path.resolve(__dirname + '/../bin/index.html')); 
-  res.sendFile(path.join(__dirname, '../bin', 'index.html'));
+  res.sendFile(path.resolve(__dirname + '/../bin/index.html')); 
+  //res.sendFile(path.join(__dirname, '../bin', 'index.html'));
   
  //res.render(path.resolve(__dirname + '/../bin/index.html'));
 }); 
+
 
 //https://medium.com/@lowewenzel/serving-express-with-a-react-single-page-app-within-the-same-application-c168f1c44201
  
